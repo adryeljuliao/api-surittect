3bf67fd140e23ce413ab8f8d649afe3bd1e36db6
Adryel Julião <adryel.juliao1@gmail.com>
2020-10-21 13:15:20 -0300

    Commit inicial

---
+++ src/main/java/com/example/surittec/SurittecApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.surittec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurittecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurittecApiApplication.class, args);
+	}
+
+}
